986105ad1d3962ff6cb66c57cc50278f203b46d7
Карэн Саркисян <gugo@MacBook-Pro-Karen.local>
2022-10-23 08:14:39 +0200

    Task13

---
+++ Task_13/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите число: ");
+int input = Convert.ToInt32(Console.ReadLine());
+int thirdDigit = GetThirdDigit(input);
+
+int GetThirdNumber(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        number /= 10;
+        count++;
+    }
+    return count;
+}
+
+int GetThirdDigit(int number)
+{
+    int length = GetThirdNumber(number);
+    int[] array = new int[length];
+
+    for (int i = length - 1; i >= 0; i--)
+    {
+        array[i] = number % 10;
+        number /= 10;
+    }
+    Console.WriteLine($"Третья цифра числа - {array[2]}");
+    return 6;
+}
